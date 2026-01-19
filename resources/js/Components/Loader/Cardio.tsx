1bdead5ff1ff82e967e406bdd9bb0e03cdc86b20
Jesús García Ortuño <jesuscnnbs@gmail.com>
2026-01-19 12:45:46 +0100

    add loader component

---
+++ resources/js/Components/Loader/Cardio.tsx
@@ -0,0 +1,17 @@
+import { Cardio } from 'ldrs/react'
+import 'ldrs/react/Cardio.css'
+
+// Default values shown
+const CardioLoader = () => {
+  return (
+    <Cardio
+      size="50"
+      stroke="4"
+      speed="2"
+      color="black"
+    />
+  )
+}
+
+
+export default CardioLoader;
